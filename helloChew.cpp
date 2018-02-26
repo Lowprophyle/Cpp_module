894e82f21f90cc95bab4fd81bd1efca11a45918f
Lowprophyle <khamanconsolidate@gmail.com>
2018-02-26 21:56:40 +0800

    Lecture 1 + Testing on how to build and run c++ program using VS code

---
+++ helloChew.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include "myHeader.h"
+int main() {
+
+    int a = 1, b = 1;
+    while (a < 10) {
+        printf ("a = %d, b = %d\n", a, b);
+        printf ("max = %d\n", max(a++, b++));
+        
+    }
+}
---
+++ myHeader.h
@@ -0,0 +1,7 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+#define max(a,b) (((a)> (b)) ? (a) : (b))
+
+
+#endif 
